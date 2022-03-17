e738c2b4a80ff61ac0f8cd0c5719b64027b7578c
Tasnima <64172539+tasnima22@users.noreply.github.com>
2022-03-17 16:31:35 +0000

    Add files via upload

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,22 +1,38 @@
 const router = require("express").Router();
+const { findByIdAndUpdate } = require("../db");
+const Person = require("../db");
 
 const data = [];
 
-router.use((req, res, next) => {
-    console.log(req.method,req.url, new Date())
-    return next(); //next -> app.post(...)
-});
-
-
-router.post('/create', (req, res) => {
+router.post('/create', (req, res, next) => {
     const name = req.body.name;
-    names.push(name);
-    res.status(201).send(`${name} added successfully`);
+    new Person(name).save().then(() => {
+        res.status(201).send(`${name} added successfully`);
+    }).catch(err => next({staus: 400, message: err.message}))
 });
-let names = ['Kiera', 'Connor', 'Callum', 'Aidan'];
-router.get('/getAll', (req, res) => res.send(names));
+    
+router.get("/getAll", (req, res, next) => {
+    Person.find((err, name) => {
+        if(err)
+        return next({status: 400, message: err.message});
+        else
+        return res.json(name);
+    })
+});
 
-router.get('/get/:id', (req, res) => res.send(names[req.params.id]));
+router.get("/get/:id", (req, res, next) => {
+    const id = req.params.id;
+    Person.findById(id, (err, found) => {
+    if (err)
+        return next ({status: 400, message: err.message});
+    else if (!found)
+        return next({ status: 404, message: "No person with this id: " + id});
+    else
+        return res.send(found);
+
+    });
+})
+
 
 // router.post('/replace/:index', (req, res) => {
 //     const name = req.query.name;
@@ -29,10 +45,19 @@ router.get('/get/:id', (req, res) => res.send(names[req.params.id]));
 router.put('/replace/:id', (req, res) => {
     const newName = req.query;
     const id = req.params.id;
-    data.splice(id, 1, newName);
-    res.status(202).json(data[id]);
-
+    Person.findByIdAndUpdate(id, newName, (err, replaced) =>
+    {
+        if (err)
+        return next ({status: 400, message: err.message});
+        else
+        return res.status(202).send(replaced);
+    })  
 });
+
+//     data.splice(id, 1, newName);
+//     res.status(202).json(data[id]);
+
+// });
 
 // router.get('/delete/:id', (req, res) => {
 //     res.send(names.splice(req.params.id, 1));
@@ -40,9 +65,16 @@ router.put('/replace/:id', (req, res) => {
 
 router.delete("/delete/:id", (req, res) => {
     const id = req.params.id;
-    data.splice(id, 1);
-    res.sendStatus(204);
+    Person.findByIdAndDelete(id,(err) => {
+        if (err)
+            return next({status: 400, mesage: err.message});
+        else
+            return res.sendStatus(204);
+    })
+});
+//     data.splice(id, 1);
+//     res.sendStatus(204);
 
-})
+// })
 
 module.exports = router;
